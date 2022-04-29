b186861321639a6d59de85d8ebebbe1dc3259278
esmuellert <41042490+esmuellert@users.noreply.github.com>
2022-04-29 14:56:55 -0400

    Update install_zsh.sh

--- install_zsh.sh
+++ install_zsh.sh
@@ -4,3 +4,4 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 wget https://raw.githubusercontent.com/esmuellert/material-deep-ocean-zsh/main/.zshrc -O $HOME/.zshrc
 wget https://github.com/esmuellert/material-deep-ocean-zsh/raw/main/material_deep_ocean.zsh-theme -O ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/material_deep_ocean.zsh-theme
+sudo chsh -s $(which zsh) $USER
