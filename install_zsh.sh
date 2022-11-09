fad84e8de28a33ac07f06ecdd258f182a674787d
esmuellert <41042490+esmuellert@users.noreply.github.com>
2022-11-08 21:19:54 -0500

    Update install_zsh.sh

--- install_zsh.sh
+++ install_zsh.sh
@@ -1,3 +1,4 @@
+sudo apt-get update
 sudo apt-get install -y zsh
 wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh || true
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
