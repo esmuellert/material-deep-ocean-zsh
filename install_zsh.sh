fcd12ac1f56bb09106b11c4a69727a75eb18e541
esmuellert <41042490+esmuellert@users.noreply.github.com>
2022-11-09 15:51:51 -0500

    Update install_zsh.sh

--- install_zsh.sh
+++ install_zsh.sh
@@ -1,8 +1,8 @@
-sudo apt-get update
-sudo apt-get install -y zsh
+#sudo apt-get update
+#sudo apt-get install -y zsh
 wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh || true
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 wget https://raw.githubusercontent.com/esmuellert/material-deep-ocean-zsh/main/.zshrc -O $HOME/.zshrc
 wget https://github.com/esmuellert/material-deep-ocean-zsh/raw/main/material_deep_ocean.zsh-theme -O ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/material_deep_ocean.zsh-theme
-sudo chsh -s $(which zsh) $USER
+#sudo chsh -s $(which zsh) $USER
